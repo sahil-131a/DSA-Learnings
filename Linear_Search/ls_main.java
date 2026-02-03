676ad84f48ab8101d73d47e8ca8164e5c6cab981
sahil-131a <sahilkharbanda131@gmail.com>
2026-02-03 23:53:45 +0530

    The actual program of the linear-Search

--- Linear_Search/ls_main.java
+++ Linear_Search/ls_main.java
@@ -1,6 +1,33 @@
+package Linear_Search;
 
 public class ls_main {
     public static void main(String[] args) {
-        System.out.print("sahil");
+        int [] arr = {1 , 13 , 20 , 31 , 30 , 11 , 16 , 8};
+        int target = 11 ;
+        int answer = linear_search_ls(arr , target);
+        if (answer == -1 ){
+            System.out.println("The element is not present or the length of array is 0");
+        }
+        else{
+            System.out.print(answer);
+        }
+
+    }
+
+    // Search in the array : and return the index if the element is found 
+    // if the element is not found or the length of array is 0 in that case it will gonna return -1 
+
+    static int linear_search_ls(int arr[] , int target){
+        if (arr.length == 0){
+            return -1;
+        }
+
+        // run a for loop to find the target
+        for(int index = 0 ; index < arr.length ; index ++){
+            if (target == arr[index]){
+                return index;
+            }
+        }
+        return -1;
     }
 }
